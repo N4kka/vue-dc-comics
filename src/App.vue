e86928e746eec2db8c27adf5d2149bcf04102068
Nicola Sarli <you@example.com>
2022-05-17 18:01:03 +0200

    Header added

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,39 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- HEADER -->
+    <AppHeader />
+    <!-- /HEADER -->
+
+    <!-- MAIN -->
+    <!-- /MAIN -->
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    AppHeader,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "./style/common.scss";
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  background-color: black;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,63 @@
+<template>
+  <header class="header">
+    <div class="header__container">
+      <img class="header__img" src="../assets/img/dc-logo.png" alt="DC logo" />
+      <ul class="header__list">
+        <li>Character</li>
+        <li id="selected">Comics</li>
+        <li>Movies</li>
+        <li>TV</li>
+        <li>Games</li>
+        <li>Collectibles</li>
+        <li>Videos</li>
+        <li>Fans</li>
+        <li>News</li>
+        <li>Shop</li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.header {
+  width: 100%;
+  height: 100px;
+  margin-top: 2rem;
+  background-color: white;
+
+  &__container {
+    width: 60%;
+    height: 100%;
+    line-height: 100px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &__img {
+    height: 75%;
+    display: flex;
+    align-self: center;
+  }
+
+  &__list {
+    display: flex;
+    list-style: none;
+    li {
+      font-weight: 500;
+      margin-right: 15px;
+    }
+    #selected {
+      color: #0282f9;
+      border-bottom: 3px solid #0282f9;
+    }
+  }
+}
+</style>
